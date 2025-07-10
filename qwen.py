b41be38cb93b35ac702f47c5b5c9e9b4a403b0d2
shaivimalik <shaivimalik27@gmail.com>
2025-07-11 00:22:08 +0530

    add qwen eval code

---
+++ qwen.py
@@ -0,0 +1,102 @@
+import os
+import torch
+import argparse
+import numpy as np
+import pandas as pd
+import torch.nn.functional as F
+from qwen_vl_utils import process_vision_info
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+
+parser = argparse.ArgumentParser(description="Evaluate Qwen2.5-VL-3B-Instruct.")
+parser.add_argument("--img_dir", type=str, required=True, help="Directory containing images.")
+parser.add_argument("--output_dir", type=str, required=True, help="Path to output directory where results will be saved.")
+args = parser.parse_args()
+
+os.makedirs(args.output_dir, exist_ok=True)
+
+df = pd.read_csv('selected_trait_words.csv')
+trait_words = df['WORD_ENGLISH'].tolist()
+
+img_paths = pd.read_csv('selected_images.csv')['file'].tolist()
+
+model = Qwen2_5_VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct", torch_dtype=torch.bfloat16, device_map="auto")
+
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
+
+yes_token_id = processor.tokenizer.encode("YES", add_special_tokens=False)[0]
+no_token_id = processor.tokenizer.encode("NO", add_special_tokens=False)[0]
+
+question_templates = [
+            "Is the person in the image {}?",
+            "Do you think the person is {}?",
+            "Does the person look {}?",
+            "Based on the image, is the person {}?",
+            "Looking at this image, are they {}?"
+            ]
+
+result = {'img':[], 'response':[], 'yes_prob':[], 'no_prob':[]}
+
+for trait in trait_words:
+    for img_file in img_paths:
+        response = np.empty(5, dtype='object')
+        yes_prob = np.zeros(5)
+        no_prob = np.zeros(5)
+        for idx, template in enumerate(question_templates):
+            question = template.format(trait)
+            messages = [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image",
+                            "image": "file:///"+os.path.join(args.img_dir, img_file)
+                        },
+                        {"type": "text", "text": f"Instructions: Analyze the provided image and answer the following question with exactly one of these responses: YES or NO.\
+                                                Question: {question}\
+                                                Response format: YES or NO"},
+                    ],
+                }
+            ]
+
+            text = processor.apply_chat_template(
+                messages, tokenize=False, add_generation_prompt=True
+            )
+            image_inputs, video_inputs = process_vision_info(messages)
+            inputs = processor(
+                text=[text],
+                images=image_inputs,
+                videos=video_inputs,
+                padding=True,
+                return_tensors="pt",
+            )
+            inputs = inputs.to("cuda")
+
+            with torch.no_grad():
+                outputs = model(**inputs)
+
+                last_token_logits = outputs.logits[:, -1, :]
+
+                probs = F.softmax(last_token_logits, dim=-1)
+
+                probability_of_yes = probs[0, yes_token_id].item()
+                yes_prob[idx] = probability_of_yes*100
+                probability_of_no = probs[0, no_token_id].item()
+                no_prob[idx] = probability_of_no*100
+
+                generated_ids = model.generate(**inputs, max_new_tokens=10)
+                generated_ids_trimmed = [
+                    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+                ]
+                output_text = processor.batch_decode(
+                    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+                )[0]
+                response[idx] = output_text
+        result["response"].append(response)
+        result['no_prob'].append(no_prob)
+        result['yes_prob'].append(yes_prob)
+        result["img"].append(img_file)
+    
+    res_df = pd.DataFrame(result)
+    expanded_cols = [res_df[col].apply(pd.Series).add_prefix(f"{col}_") for col in ['response', 'no_prob', 'yes_prob']]
+    res_df_final = pd.concat([res_df.drop(columns=['response', 'no_prob', 'yes_prob'])] + expanded_cols, axis=1)
+    res_df_final.to_csv(os.path.join(args.output_dir, f'Qwen2.5-VL-3B-Instruct_{trait}.csv'), index=False)             
